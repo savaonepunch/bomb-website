0cb557602ac1bb98f12ab0bf7c9aac3e59a1c1fa
Sava <87912493+savaonepunch@users.noreply.github.com>
2022-04-30 19:45:00 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 window.addEventListener('load', () => {
-    /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
     particlesJS.load('particles-js', 'particles.json', function () {
         console.log('callback - particles.js config loaded');
     });
 
     const cards = document.querySelectorAll('.card');
+    const wrapper = document.getElementById('containerId');
 
     cards.forEach(card => {
         card.addEventListener('click', function () {
@@ -13,12 +13,37 @@ window.addEventListener('load', () => {
     });
 
     document.getElementById('discord').addEventListener('click', () => {
-        navigator.clipboard.writeText("Placeholder#0000").then(function () {
+        navigator.clipboard.writeText("LittleGuy252#7657").then(function () {
             console.log('done');
             alert("Discord copied to clipboard!");
         }, function () {
             console.log('error');
             alert("Couldn't copy discord to clipboard!");
         });
+    })
+
+    // Change the "LEARN MORE" URLs here
+    wrapper.addEventListener('click', (event) => {
+        const isButton = event.target.nodeName === 'BUTTON';
+        if (!isButton) {
+            return;
+        }
+
+        switch (event.target.id) {
+            case 'service1':
+                window.open("https://www.google.com", "_blank");
+                break;
+            case 'service2':
+                window.open("https://www.google.com", "_blank");
+                break;
+            case 'service3':
+                window.open("https://www.google.com", "_blank");
+                break;
+            case 'service4':
+                window.open("https://www.google.com", "_blank");
+                break;
+            default:
+                break;
+        }
     })
 })
